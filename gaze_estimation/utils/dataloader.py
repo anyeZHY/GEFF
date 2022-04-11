2814aa813656f5a1c60d73cf0e4fed6aba502cf3
anyeZHY <47559723+anyeZHY@users.noreply.github.com>
2022-04-11 16:38:02 +0800

    Update dataloader.py

--- gaze_estimation/utils/dataloader.py
+++ gaze_estimation/utils/dataloader.py
@@ -6,34 +6,14 @@ import torch
 from torchvision.io import read_image
 from torch.utils.data import Dataset
 from torchvision import transforms
+# from sklearn.utils import shuffle
 import torch.utils.data
 
 datapath = 'assets/MPIIFaceGaze/'
-colomn = ['Face', 'Left', 'Right', '3DGaze', '2DGaze']
+colomn = ['Face', 'Left', 'Right', '3DGaze', '2DGaze', '3DHead', '2DHead']
 
-# unused now
-def procees_data(save = 1):
-    data = pd.DataFrame(columns=colomn)
-    for i_label in range(10):
-        labelpath = datapath + 'Label/p' + str(i_label).zfill(2) + '.label'
-        df = pd.read_table(labelpath, delimiter=' ')
-        # df = df.head()
-        df = df[colomn]
-        # print(len(df))
-        for i_pic in range(len(df)):
-            for col in ['Face', 'Left', 'Right']:
-                facepath = datapath + 'Image/' + df[col][i_pic].replace('\\','/')
-                im = np.array(Image.open(facepath))
-                df[col][i_pic] = im
-            for col in ['3DGaze', '2DGaze']:
-                arr = np.array(list(map(float, df[col][i_pic].split(','))))
-                df[col][i_pic] = arr
-        data = pd.concat([data, df])
-    if save:
-        data.to_pickle('assets/MPIIFaceGazeData.csv')
-    return data
-
-def extract_from_data(save = 1):
+# ============== Process .label >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def split_data(test_length = 1000):
     data = pd.DataFrame(columns=colomn)
     for i_label in range(10):
         labelpath = datapath + 'Label/p' + str(i_label).zfill(2) + '.label'
@@ -46,20 +26,21 @@ def extract_from_data(save = 1):
                 facepath = df[col][i_pic].replace('\\','/')
                 # im = np.array(Image.open(facepath))
                 df[col][i_pic] = facepath
-            for col in ['3DGaze', '2DGaze']:
-                arr = np.array(list(map(float, df[col][i_pic].split(','))))
-                df[col][i_pic] = arr
+            # for col in ['3DGaze', '2DGaze', '3dHead', '2DHead']:
+            #     arr = np.array(list(map(float, df[col][i_pic].split(','))))
+            #     df[col][i_pic] = arr
         data = pd.concat([data, df])
-    if save:
-        data[['Face', '3DGaze']].to_pickle('assets/MPII_3D_annoataion.csv')
-        data[['Face', '2DGaze']].to_pickle('assets/MPII_2D_annoataion.csv')
+    data = data[colomn].sample(frac = 1).reset_index(drop=True)
+    # print(data.head())
+    data.head(test_length).to_csv('assets/MPII_test.csv')
+    data.tail(len(data)-test_length).reset_index(drop=True).to_csv('assets/MPII_data.csv')
     return data
 
 # ============== Data Process >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # Reference: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files
 class MPII(Dataset):
     def __init__(self, annotations_file, img_dir, transform=None, target_transform=None):
-        self.img_labels = pd.read_pickle(annotations_file)
+        self.img_labels = pd.read_csv(annotations_file)
         self.img_dir = img_dir
         self.transform = transform
         self.target_transform = target_transform
@@ -68,9 +49,11 @@ class MPII(Dataset):
         return len(self.img_labels)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.img_labels.iloc[idx, 0])
+        # print(self.img_labels['Face'].iloc[idx])
+        img_path = os.path.join(self.img_dir, self.img_labels['Face'].iloc[idx])
         image = read_image(img_path)
-        label = self.img_labels.iloc[idx, 1]
+        # label = self.img_labels.iloc[idx, 1]
+        label = np.array(list(map(float, self.img_labels['2DGaze'].iloc[idx].split(','))))
         if self.transform:
             image = self.transform(image)
         if self.target_transform:
@@ -80,7 +63,9 @@ class MPII(Dataset):
 def load_data_naive(BATCH_SIZE):
     # df_data = procees_data(0)
     # df_data = pd.read_pickle('assets/MPII_2D_annoataion.csv')
-    annotations_file = 'assets/MPII_2D_annoataion.csv'
+    if not os.path.isfile('assets/MPII_3D_test.csv'):
+        split_data()
+    annotations_file = 'assets/MPII_data.csv'
     img_dir = 'assets/MPIIFaceGaze/Image'
 
     transform_train = transforms.Compose([
@@ -100,14 +85,13 @@ def load_data_naive(BATCH_SIZE):
 
     data = MPII(annotations_file, img_dir, transform_train)
 
-    train_set, val_set = torch.utils.data.random_split(data, [25000, 5000])
+    train_set, val_set = torch.utils.data.random_split(data, [len(data) - 5000, 5000])
 
     trainloader = torch.utils.data.DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True)
-
     testloader = torch.utils.data.DataLoader(val_set, batch_size=100, shuffle=True)
+
     return trainloader, testloader
 
 if __name__ == '__main__':
     # load_data_naive(128)
-    # procees_data()
-    extract_from_data()
+    split_data()
