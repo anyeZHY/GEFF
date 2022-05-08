64f1535effc85a582b74a27e35a873dd658948dc
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-08 21:29:16 +0800

    Update loss_plot relevant files

--- ge/utils/loss_plot.py
+++ ge/utils/loss_plot.py
@@ -5,21 +5,24 @@ import matplotlib.pyplot as plt
 import os
 
 
-def generate_loss_logs(files):
+def generate_loss_logs():
     """
         The file tree should be the same as project GE
     """
-
-    path = str(Path.cwd().parent.parent)
+    path = str(Path.cwd().parent.parent)+'/log/'
+    files = os.listdir(path)
     logs = []
+    file_names = []
     for file in files:
-        loss_log = []
-        df = pd.read_table(path + file, header=None, skiprows=0)
-        for i in range(len(df)):
-            if str(df[0][i])[0:4] == "Test":
-                loss_log.append(float(str(df[0][i])[16:]))
-        logs.append(loss_log)
-    return logs
+        if file.endswith(".txt"):
+            loss_log = []
+            df = pd.read_table(path + file, header=None, skiprows=0)
+            for i in range(len(df)):
+                if str(df[0][i])[0:4] == "Test":
+                    loss_log.append(float(str(df[0][i])[16:]))
+            logs.append(loss_log)
+            file_names.append(file[:-4])
+    return logs, file_names
 
 def smooth_log(logs):
     logs = np.array(logs)
@@ -33,33 +36,28 @@ def smooth_log(logs):
         result.append(subresult)
     return result
 
-def plot_loss(files, logs):
-    plt.title("Test losses with various models")
+def plot_loss_one_graph(files, logs):
+    plt.title("Validation losses with various models")
     for i in range(len(files)):
-        plt.plot(logs[i], label=files[i][5:-4], linewidth=1.1, )
+        plt.plot(logs[i], label=files[i], linewidth=1.1, )
     plt.legend()
     plt.xlabel('Train epoches')
-    plt.ylabel('Test loss')
+    plt.ylabel('Validation loss')
     # plt.show()
-    plt.savefig('../../figs/Test_losses.pdf')
+    plt.savefig('../../figs/Validation_losses.pdf')
 
 
 def generate_log_files(file_dir):
+
     pass
 
 
 if __name__ == '__main__':
-    log_files = [
-        '/log/baseline.txt',
-        '/log/naive fuse.txt',
-        '/log/fuse lr=1e-4.txt',
-        '/log/fuse lr=5e-4.txt'
-    ]
-    loss_logs = generate_loss_logs(log_files)
+    loss_logs, file_names = generate_loss_logs()
     loss_logs = np.array(loss_logs)
     # start = 34
     # loss_logs = smooth_log(loss_logs[:, start:])
-    plot_loss(log_files, loss_logs)
+    plot_loss_one_graph(file_names, loss_logs)
 
 
 
