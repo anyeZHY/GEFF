3cb003cccf1ba20a6fdba02b408ae79162512b5e
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-11 11:01:46 +0800

    Update loss_analysis.py

--- ge/utils/loss_analysis.py
+++ ge/utils/loss_analysis.py
@@ -6,8 +6,10 @@ import os
 
 
 def generate_loss_logs():
-    logs = []
-    names = []
+    logs1 = []
+    names1 = [] # store 100 training epoch
+    logs2 = []
+    names2 = [] # store 200 training epoch
     path = str(Path.cwd()) + '/log/base/'
     files = os.listdir(path)
     for file in files:
@@ -17,8 +19,12 @@ def generate_loss_logs():
             for i in range(len(df)):
                 if str(df[0][i])[0:4] == "Test":
                     loss_log.append(float(str(df[0][i])[16:]))
-            logs.append(loss_log)
-            names.append(file[:-4])
+            if len(loss_log) == 100:
+                logs1.append(loss_log)
+                names1.append(file[:-4])
+            if len(loss_log) == 200:
+                logs2.append(loss_log)
+                names2.append(file[:-4])
     path = str(Path.cwd()) + '/log/fuse/'
     files = os.listdir(path)
     for file in files:
@@ -28,8 +34,12 @@ def generate_loss_logs():
             for i in range(len(df)):
                 if str(df[0][i])[0:4] == "Test":
                     loss_log.append(float(str(df[0][i])[16:]))
-            logs.append(loss_log)
-            names.append(file[:-4])
+            if len(loss_log) == 100:
+                logs1.append(loss_log)
+                names1.append(file[:-4])
+            if len(loss_log) == 200:
+                logs2.append(loss_log)
+                names2.append(file[:-4])
     path = str(Path.cwd()) + '/log/geff/'
     files = os.listdir(path)
     for file in files:
@@ -39,9 +49,13 @@ def generate_loss_logs():
             for i in range(len(df)):
                 if str(df[0][i])[0:4] == "Test":
                     loss_log.append(float(str(df[0][i])[16:]))
-            logs.append(loss_log)
-            names.append(file[:-4])
-    return logs, names
+            if len(loss_log) == 100:
+                logs1.append(loss_log)
+                names1.append(file[:-4])
+            if len(loss_log) == 200:
+                logs2.append(loss_log)
+                names2.append(file[:-4])
+    return logs1, names1, logs2, names2
 
 
 def smooth_log(logs):
@@ -81,7 +95,7 @@ def plot_loss_multi_graph(files, logs):
         plt.savefig(f'../../figs/loss/{files[i]}.pdf')
 
 
-def show_mean_loss(files, loss, position):
+def show_mean_loss_archive(files, loss, position):
     path = str(Path.cwd().parent.parent) + '/log/'
     mean = np.mean(loss[:, position:], axis=1)
     print(f'mean validation loss on epoches {position} to 100')
@@ -90,24 +104,32 @@ def show_mean_loss(files, loss, position):
         print(round(mean[i], 4), '\n', df[0][1], '\n')
 
 
-def show_mean_loss_2(files, loss, position1, position2):
+def show_mean_loss(files, loss, length):
     path = str(Path.cwd()) + '/log/'
+    pos = np.argmin(loss, axis=1)
+    # pos60 = min(pos, length-60)
+    # pos20 = min(pos, length-20)
     min = np.min(loss, axis=1)
-    mean1 = np.mean(loss[:, position1:], axis=1)
-    mean2 = np.mean(loss[:, position2:], axis=1)
-    print(f'mean loss on epoches {position1} to 100 and {position2} to 100 and global min loss')
+    pos60 = length-60
+    pos20 = length-20
+    mean60 = np.mean(loss[:, pos60:], axis=1)
+    mean20 = np.mean(loss[:, pos20:], axis=1)
+    print(f'mean loss on epoches {pos60} to {length} and {pos20} to {length} and global min loss')
     for i in range(len(files)):
         df = pd.read_table(path + files[i][:4] +'/' + files[i] + '.txt', header=None, skiprows=0)
-        print('min', round(min[i], 4),'(40,100)', round(mean1[i], 4), '(80,100)', round(mean2[i], 4), files[i], '\n', df[0][1], '\n')
+        print('min', round(min[i], 4), round(mean60[i], 4), round(mean20[i], 4), files[i], '\n', df[0][1], '\n')
 
 
 if __name__ == '__main__':
-    loss_logs, file_names = generate_loss_logs()
-    loss_logs = np.array(loss_logs)
-    # print(len(file_names))
-    # print(len(loss_logs))
+    loss100, file100, loss200, file200 = generate_loss_logs()
+    loss100 = np.array(loss100)
+    loss200 = np.array(loss200)
+    pos1 = np.argmin(loss100, axis=1)
+    pos2 = np.argmin(loss200, axis=1)
+    # print(pos1, pos2)
     # start = 34
     # loss_logs = smooth_log(loss_logs[:, start:])
     # plot_loss_one_graph(file_names, loss_logs)
     # plot_loss_multi_graph(file_names, loss_logs)
-    show_mean_loss_2(file_names, loss_logs, 40, 80)
+    # show_mean_loss(file100, loss100, 100)
+    # show_mean_loss(file200, loss200, 200)
