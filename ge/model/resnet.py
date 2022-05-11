00556d8a7c3490c869562abb712c4ee88f855a82
anyeZHY <47559723+anyeZHY@users.noreply.github.com>
2022-05-11 19:25:06 +0800

    Update resnet.py

--- ge/model/resnet.py
+++ ge/model/resnet.py
@@ -163,6 +163,7 @@ class ResNet(nn.Module):
             width_per_group: int = 64,
             replace_stride_with_dilation: Optional[List[bool]] = None,
             norm_layer: Optional[Callable[..., nn.Module]] = None,
+            channel = None
     ) -> None:
         super().__init__()
         if norm_layer is None:
@@ -186,10 +187,12 @@ class ResNet(nn.Module):
         self.bn1 = norm_layer(self.inplanes)
         self.relu = nn.ReLU(inplace=True)
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
-        self.layer1 = self._make_layer(block, 64, layers[0])
-        self.layer2 = self._make_layer(block, 128, layers[1], stride=2, dilate=replace_stride_with_dilation[0])
-        self.layer3 = self._make_layer(block, 256, layers[2], stride=2, dilate=replace_stride_with_dilation[1])
-        self.layer4 = self._make_layer(block, 512, layers[3], stride=2, dilate=replace_stride_with_dilation[2])
+        if channel is None:
+            channel = [64, 128, 256, 512]
+        self.layer1 = self._make_layer(block, channel[0], layers[0])
+        self.layer2 = self._make_layer(block, channel[1], layers[1], stride=2, dilate=replace_stride_with_dilation[0])
+        self.layer3 = self._make_layer(block, channel[2], layers[2], stride=2, dilate=replace_stride_with_dilation[1])
+        self.layer4 = self._make_layer(block, channel[3], layers[3], stride=2, dilate=replace_stride_with_dilation[2])
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
