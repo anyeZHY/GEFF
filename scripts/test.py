8d2e6d91dfbadb75604c5480beeb262106b45a09
anyeZHY <47559723+anyeZHY@users.noreply.github.com>
2022-05-16 20:52:10 +0800

    Update SimCLR

---
+++ scripts/test.py
@@ -0,0 +1,6 @@
+# To Do:
+# 1. Get angular loss on our test set
+# 2. Provide a method to compute the angular loss on ungiven datas
+# 3. Compute angluar loss on another dataset (the domain of labels is quite differnent)
+
+
