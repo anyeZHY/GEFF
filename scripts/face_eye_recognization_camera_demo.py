4c74f2bee588c2a353e073a3e40320c4afd7fead
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-19 19:06:40 +0800

    Update face_eye_recognization_camera_demo.py
    将路径 path 修改为可在各台电脑上适用

--- scripts/face_eye_recognization_camera_demo.py
+++ scripts/face_eye_recognization_camera_demo.py
@@ -4,6 +4,7 @@
 # import libraries of python OpenCV
 # where its functionality resides
 import cv2
+import os
 
 # load the required trained XML classifiers
 # https://github.com/Itseez/opencv/blob/master/
@@ -12,19 +13,19 @@ import cv2
 # object we want to detect a cascade function is trained
 # from a lot of positive(faces) and negative(non-faces)
 # images.
-path = '/Users/anye_zhenhaoyu/Desktop/机器学习/Project/Gaze_Estimation'
-face_cascade = cv2.CascadeClassifier( path + '/assets/haarcascade_frontalface_default.xml' )
+path = os.getcwd()
+face_cascade = cv2.CascadeClassifier(path + '/assets/haarcascade_frontalface_default.xml' )
 
 # https://github.com/Itseez/opencv/blob/master
 # /data/haarcascades/haarcascade_eye.xml
 # Trained XML file for detecting eyes
-eye_cascade = cv2.CascadeClassifier( path + '/assets/haarcascade_eye.xml' )
+eye_cascade = cv2.CascadeClassifier(path + '/assets/haarcascade_eye.xml')
 
 # capture frames from a camera
 cap = cv2.VideoCapture(0)
 
 # loop runs if capturing has been initialized.
-while 1:
+while True:
 
     # reads frames from a camera
     ret, img = cap.read()
@@ -32,13 +33,11 @@ while 1:
     L = img.shape[1]
     a = 200
     img = img[int(W / 2) - a:int(W / 2) + a, int(L / 2) - a:int(L / 2) + a, :]
-
     # convert to gray scale of each frames
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
     # Detects faces of different sizes in the input image
     faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-
     for (x, y, w, h) in faces:
         # To draw a rectangle in a face
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
@@ -47,7 +46,6 @@ while 1:
 
         # Detects eyes of different sizes in the input image
         # eyes = eye_cascade.detectMultiScale(roi_gray)
-
         # To draw a rectangle in eyes
         # for (ex, ey, ew, eh) in eyes:
         #     cv2.rectangle(roi_color, (ex, ey), (ex + ew, ey + eh), (0, 127, 255), 2)
