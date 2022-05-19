e73227341210f27c02b765bd281304102e140ebc
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-19 13:14:26 +0800

    Update eye_cut.py
    Not using the cv2 library

--- gaze/utils/eye_cut.py
+++ gaze/utils/eye_cut.py
@@ -24,20 +24,24 @@ def create_folders():
 
 
 def cut_eye():
-    path = os.getcwd() + '/assets/ColumbiaGazeCutSet/'
+    path = os.getcwd() + '/assets/ColumbiaGazeDataSet/'
     folder = os.listdir(path)
     for curr_dir in folder:
+        print(curr_dir)
         if curr_dir == '.DS_Store':
             continue
-        files = os.listdir(path+curr_dir+'/face')
+        files = os.listdir(path+curr_dir+'/')
         for file in files:
-            img = read_image(path+curr_dir+'/face/'+file)
-            left = F.crop(img, 50, 30, 36, 60)
-            right = F.crop(img, 50, 115, 36, 60)
-            left_gray = T.Grayscale()(left).detach().numpy()
-            right_gray = T.Grayscale()(right).detach().numpy()
-            cv2.imwrite(path+curr_dir+'/left/'+file, left_gray.reshape(36, 60, 1))
-            cv2.imwrite(path+curr_dir+'/right/'+file, right_gray.reshape(36, 60, 1))
+            if file.endswith(".jpg"):
+                img = read_image(path+curr_dir+'/'+file)
+                left = F.resized_crop(img, 1444, 1945, 360, 600, [36, 60])
+                right = F.resized_crop(img, 1444, 2591, 360, 600, [36, 60])
+                left_gray = T.Grayscale()(left).detach().numpy()
+                right_gray = T.Grayscale()(right).detach().numpy()
+                cv2.imwrite(os.getcwd() + '/assets/ColumbiaGazeCutSet/'+curr_dir+'/left/'+file,
+                            left_gray.reshape(36, 60, 1))
+                cv2.imwrite(os.getcwd() + '/assets/ColumbiaGazeCutSet/'+curr_dir+'/right/'+file,
+                            right_gray.reshape(36, 60, 1))
 
 
 if __name__ == '__main__':
