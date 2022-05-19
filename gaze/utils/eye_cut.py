3b55898d4926cae7520b283d2d6ac4233c5d47c1
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-19 21:52:09 +0800

    Update eye_cut.py

--- gaze/utils/eye_cut.py
+++ gaze/utils/eye_cut.py
@@ -4,6 +4,7 @@ from torchvision.io import read_image
 import torchvision.transforms as T
 import torchvision.transforms.functional as F
 import cv2
+import numpy as np
 
 
 def create_folders():
@@ -24,18 +25,32 @@ def create_folders():
 
 
 def cut_eye():
+    pos = [0,
+           0, -6, 0, 0, 0,
+           0, 0, 0, 0, 0,
+           0, 0, 0, 0, 0,
+           0, 2, 0, 3, 3,
+           6, 6, 6, 6, 6,
+           0, 0, 0, 0, 0,
+           2, 0, 2, 3, 0,
+           0, 2, 2, 2, 0,
+           0, -2, -2, 0, 0,
+           0, 2, 2, 0, -2,
+           -2, 0, 0, -3, -6,
+           -3]
+    step = 60
     path = os.getcwd() + '/assets/ColumbiaGazeDataSet/'
     folder = os.listdir(path)
     for curr_dir in folder:
-        print(curr_dir)
         if curr_dir == '.DS_Store':
             continue
+        print(int(curr_dir))
         files = os.listdir(path+curr_dir+'/')
         for file in files:
             if file.endswith(".jpg"):
                 img = read_image(path+curr_dir+'/'+file)
-                left = F.resized_crop(img, 1444, 1945, 360, 600, [36, 60])
-                right = F.resized_crop(img, 1444, 2591, 360, 600, [36, 60])
+                left = F.resized_crop(img, 1444+pos[int(curr_dir)]*step, 1945, 360, 600, [36, 60])
+                right = F.resized_crop(img, 1444+pos[int(curr_dir)]*step, 2591, 360, 600, [36, 60])
                 left_gray = T.Grayscale()(left).detach().numpy()
                 right_gray = T.Grayscale()(right).detach().numpy()
                 cv2.imwrite(os.getcwd() + '/assets/ColumbiaGazeCutSet/'+curr_dir+'/left/'+file,
@@ -44,8 +59,30 @@ def cut_eye():
                             right_gray.reshape(36, 60, 1))
 
 
-if __name__ == '__main__':
-    create_folders()  
-    cut_eye()
+# def cut_eye_cv2():
+#     eye_cascade = cv2.CascadeClassifier(os.getcwd() + '/assets/haarcascade_eye.xml')
+#     path = os.getcwd() + '/assets/ColumbiaGazeCutSet/'
+#     folder = os.listdir(path)
+#     for curr_dir in folder:
+#         print(curr_dir)
+#         if curr_dir == '.DS_Store':
+#             continue
+#         files = os.listdir(path + curr_dir + '/face/')
+#         for file in files:
+#             if file.endswith(".jpg"):
+#                 img = cv2.imread(path+curr_dir+'/face/'+file)
+#                 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+#                 eyes = eye_cascade.detectMultiScale(gray)
+#                 if len(eyes) == 2:
+#                     for (ex, ey, ew, eh) in eyes:
+#                         print(ex, ey)
+#                         print(os.getcwd() + '/assets/Columbiatest/' + curr_dir + '/' + file)
+#                         cv2.imwrite(os.getcwd() + '/assets/Columbiatest/' + curr_dir + '/' + file,
+#                                     gray[ey:ey+36, ex:ex+60])
+#         break
 
+
+if __name__ == '__main__':
+    # create_folders()
+    cut_eye()
 
