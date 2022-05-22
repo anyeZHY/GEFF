f16d9ffa07f64bc21ffac7aa2d9db9fa14547089
Sylvan <68489307+SylvanSun@users.noreply.github.com>
2022-05-22 21:51:58 +0800

    Update dataloader.py

--- gaze/utils/dataloader.py
+++ gaze/utils/dataloader.py
@@ -9,34 +9,37 @@ from torchvision import transforms
 import torch.utils.data
 
 datapath = 'assets/MPIIFaceGaze/'
-colomn = ['Face', 'Left', 'Right', '3DGaze', '2DGaze', '3DHead', '2DHead']
+column = ['Face', 'Left', 'Right', '3DGaze', '2DGaze', '3DHead', '2DHead']
+
 
 # ============== Useful functions >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def convert_str_to_float(string):
     """
-    convert the string to a darray
+    convert the string to a ndarray
     """
     return np.array(list(map(float, string.split(','))))
+
+
 def get_img(dir, path, mode='rgb'):
     path = os.path.join(dir, path)
-    if mode=='rgb':
+    if mode == 'rgb':
         return read_image(path)
     else:
         return read_image(path, mode=image.ImageReadMode.GRAY)
 
-# ============== Process .label >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+# ============== Process .label >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def split_mpii(id: int):
     """
     Split/resplit the data into training set, validation set and test set.
     """
     print("Loading Data...")
-    train = pd.DataFrame(columns=colomn)
-    val = pd.DataFrame(columns=colomn)
+    train = pd.DataFrame(columns=column)
+    val = pd.DataFrame(columns=column)
     for i_label in range(10):
         labelpath = datapath + 'Label/p' + str(i_label).zfill(2) + '.label'
         df = pd.read_table(labelpath, delimiter=' ')
-        df = df[colomn]
+        df = df[column]
         for i_pic in range(len(df)):
             for col in ['Face', 'Left', 'Right']:
                 facepath = df[col][i_pic].replace('\\','/')
@@ -45,23 +48,53 @@ def split_mpii(id: int):
             val = pd.concat([val, df])
         else:
             train = pd.concat([train, df])
-    train= train[colomn].sample(frac = 1).reset_index(drop=True)
-    val= val[colomn].sample(frac = 1).reset_index(drop=True)
+    train = train[column].sample(frac=1).reset_index(drop=True)
+    val = val[column].sample(frac=1).reset_index(drop=True)
     # train.to_csv('assets/MPII_train.csv')
     # val.to_csv('assets/MPII_val.csv')
     print("Done!")
     return train, val
 
-# ============== Data Process >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 
+# ============== Process ColumbiaDataCutSet >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+def split_columbia(id: int):
+    """
+    Split/re-split the data into training set, validation set and test set.
+    """
+    print("Loading Data...")
+    train = pd.DataFrame(columns=column)
+    val = pd.DataFrame(columns=column)
+    for i_label in range(10):
+        labelpath = datapath + 'Label/p' + str(i_label).zfill(2) + '.label'
+        df = pd.read_table(labelpath, delimiter=' ')
+        df = df[column]
+        for i_pic in range(len(df)):
+            for col in ['Face', 'Left', 'Right']:
+                facepath = df[col][i_pic].replace('\\', '/')
+                df[col][i_pic] = facepath
+        if i_label == id:
+            val = pd.concat([val, df])
+        else:
+            train = pd.concat([train, df])
+    print(val)
+    print(train)
+    train = train[column].sample(frac=1).reset_index(drop=True)
+    val = val[column].sample(frac=1).reset_index(drop=True)
+    print(val)
+    print(train)
+    print("Done!")
+    return train, val
+
+
+# ============== Data Process >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 def mask(face, eye):
     shape = eye.shape
     p = torch.rand(1)
     if 0 < p < 0.05:
         eye = torch.randn(shape)
-    elif 0.05 <= p < 0.2 :
+    elif 0.05 <= p < 0.2:
         eye = transforms.RandomCrop(shape[1:])(face)
-        eye = eye[0].reshape(1, 36 ,60)
+        eye = eye[0].reshape(1, 36, 60)
     else:
         color_jitter = transforms.ColorJitter(0.4, 0.4, 0.4, 0.1)
         gaussian_blur = transforms.GaussianBlur(kernel_size=(3, 5))
@@ -72,6 +105,7 @@ def mask(face, eye):
         ])
         eye = T(eye)
     return eye
+
 
 class MPII(Dataset):
     """
@@ -93,7 +127,7 @@ class MPII(Dataset):
 
     def __getitem__(self, idx):
         img_face = get_img(self.img_dir, self.img_labels['Face'].iloc[idx])
-        img_left= get_img(self.img_dir, self.img_labels['Left'].iloc[idx])
+        img_left = get_img(self.img_dir, self.img_labels['Left'].iloc[idx])
         img_right = get_img(self.img_dir, self.img_labels['Right'].iloc[idx])
         label = convert_str_to_float(self.img_labels['2DGaze'].iloc[idx])
         if self.transform:
@@ -103,7 +137,7 @@ class MPII(Dataset):
             img_right = self.transform_eye(img_right)
         if self.target_transform:
             label = self.target_transform(label)
-        if torch.rand(1)<self.flip:
+        if torch.rand(1) < self.flip:
             flip = transforms.RandomHorizontalFlip(1)
             label[:][0] = - label[:][0]
             img_face = flip(img_face)
@@ -117,6 +151,7 @@ class MPII(Dataset):
             'Right': img_right.float()
         }
         return images, label.astype(float)
+
 
 def make_transform():
     transform_eye = transforms.Compose([
@@ -160,5 +195,7 @@ def load_data(args, BATCH_SIZE, val_size=128, transform_train=None, flip=0, pers
     val_loader = torch.utils.data.DataLoader(val_set, batch_size=val_size, shuffle=True)
     return train_loader, val_loader
 
+
 if __name__ == '__main__':
-    split_mpii(9)
+    # split_mpii(9)
+    split_columbia(9)
